dfaae804ecd8464715d6cfedb7aa14aeedf948df
chocozero <chocozero@218.38.137.28>
2017-08-03 15:17:52 +0900

    회원정보 수정 기능 구현

--- src/main/java/chocozero/codesquad/web/UserController.java
+++ src/main/java/chocozero/codesquad/web/UserController.java
@@ -38,4 +38,28 @@ public class UserController {
 		}
 		return mav;
 	}
+	
+	@GetMapping("/users/{userId}/form")
+	public ModelAndView updateForm(@PathVariable String userId) {
+		ModelAndView mav = new ModelAndView("user/updateForm");
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getUserId().equals(userId)) {
+				mav.addObject("user", users.get(i));
+			}
+		}
+		return mav;
+	}
+	
+	@PostMapping("/users/{userId}/update")
+	public ModelAndView updateUser(@PathVariable String userId, User user) {
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getUserId().equals(userId)) {
+				users.get(i).setUserId(user.getUserId());
+				users.get(i).setPassword(user.getPassword());
+				users.get(i).setName(user.getName());
+				users.get(i).setEmail(user.getEmail());
+			}
+		}
+		return new ModelAndView("redirect:/users");
+	}
 }
